e7e546c2fe7fb5b2f4abf69ce03455710a610053
Sealion <patrik.sjolin@gmail.com>
2017-12-05 07:01:02 +0100

    Day 5

--- AdventOfCode/AdventOfCode/Days/5.cs
+++ AdventOfCode/AdventOfCode/Days/5.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Days
@@ -7,12 +8,44 @@ namespace AdventOfCode.Days
     {
         public void RunOne()
         {
-            List<string> inputLines = System.IO.File.ReadAllLines(@"..\..\input3.txt").ToList();
+            List<string> inputLines = System.IO.File.ReadAllLines(@"..\..\input5.txt").ToList();
+            List<int> input = inputLines.Select(x => int.Parse(x)).ToList();
+            int index = 0;
+            int totalJumps = 0;
+            while(index < input.Count)
+            {
+                totalJumps++;
+                int previousIndex = index;
+                index = index + input[index];
+                input[previousIndex]++;
+            }
+
+            Console.WriteLine(totalJumps);
         }
 
         public void RunTwo()
         {
-            List<string> inputLines = System.IO.File.ReadAllLines(@"..\..\input3.txt").ToList();
+            List<string> inputLines = System.IO.File.ReadAllLines(@"..\..\input5.txt").ToList();
+            List<int> input = inputLines.Select(x => int.Parse(x)).ToList();
+            int index = 0;
+            int totalJumps = 0;
+            while (index < input.Count)
+            {
+                totalJumps++;
+                int previousIndex = index;
+                index = index + input[index];
+                if (input[previousIndex] >= 3)
+                {
+
+                    input[previousIndex]--;
+                }
+                else
+                {
+                    input[previousIndex]++;
+                }
+            }
+
+            Console.WriteLine(totalJumps);
         }
     }
 }
