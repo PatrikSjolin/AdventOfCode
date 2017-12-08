df064c5101d748756a7a64ccba1b1a9fbb576e22
Sealion <patrik.sjolin@gmail.com>
2017-12-08 07:00:50 +0100

    Day 8

--- AdventOfCode/AdventOfCode/Days/Day08.cs
+++ AdventOfCode/AdventOfCode/Days/Day08.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Days
@@ -8,6 +9,127 @@ namespace AdventOfCode.Days
         public void RunOne()
         {
             List<string> inputLines = System.IO.File.ReadAllLines(@"..\..\input8.txt").ToList();
+
+            Dictionary<string, int> registers = new Dictionary<string, int>();
+            int max = 0;
+            foreach(var s in inputLines)
+            {
+                List<string> split = s.Split(' ').ToList();
+
+                string register = split[0];
+                string condReg = split[4];
+
+                if (!registers.ContainsKey(register))
+                {
+                    registers.Add(register, 0);
+                }
+                if (!registers.ContainsKey(condReg))
+                {
+                    registers.Add(condReg, 0);
+                }
+
+                int value = int.Parse(split[2]);
+
+                                string op = split[1];
+
+                string condOp = split[5];
+                int condVal = int.Parse(split[6]);
+
+                if(condOp == ">")
+                {
+                    if(registers[condReg] > condVal)
+                    {
+                        if(op == "inc")
+                        {
+                            registers[register] += value;
+                        }
+                        else
+                        {
+                            registers[register] -= value;
+                        }
+                    }
+                }
+                else if(condOp == "<")
+                {
+                    if (registers[condReg] < condVal)
+                    {
+                        if (op == "inc")
+                        {
+                            registers[register] += value;
+                        }
+                        else
+                        {
+                            registers[register] -= value;
+                        }
+                    }
+                }
+                else if(condOp == "<=")
+                {
+                    if (registers[condReg] <= condVal)
+                    {
+                        if (op == "inc")
+                        {
+                            registers[register] += value;
+                        }
+                        else
+                        {
+                            registers[register] -= value;
+                        }
+                    }
+                }
+                else if(condOp == ">=")
+                {
+                    if (registers[condReg] >= condVal)
+                    {
+                        if (op == "inc")
+                        {
+                            registers[register] += value;
+                        }
+                        else
+                        {
+                            registers[register] -= value;
+                        }
+                    }
+                }
+                else if(condOp == "==")
+                {
+                    if (registers[condReg] == condVal)
+                    {
+                        if (op == "inc")
+                        {
+                            registers[register] += value;
+                        }
+                        else
+                        {
+                            registers[register] -= value;
+                        }
+                    }
+                }
+                else if(condOp == "!=")
+                {
+                    if (registers[condReg] != condVal)
+                    {
+                        if (op == "inc")
+                        {
+                            registers[register] += value;
+                        }
+                        else
+                        {
+                            registers[register] -= value;
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+
+                if(registers.Values.Max() > max)
+                {
+                    max = registers.Values.Max();
+                }
+            }
+            Console.WriteLine(max);
         }
 
         public void RunTwo()
--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -19,7 +19,7 @@ namespace AdventOfCode
                 new Day05(),
                 new Day06(),
                 new Day07(),
-                //new Day08(),
+                new Day08(),
                 //new Day09(),
                 //new Day10(),
                 //new Day011(),
