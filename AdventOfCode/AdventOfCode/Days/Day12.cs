835f1565706bf821d0e9e394c86d22d645ca17e7
Sealion <patrik.sjolin@gmail.com>
2017-12-12 07:09:47 +0100

    Day 12

--- AdventOfCode/AdventOfCode/Days/Day12.cs
+++ AdventOfCode/AdventOfCode/Days/Day12.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Days
@@ -8,11 +9,107 @@ namespace AdventOfCode.Days
         public void RunOne()
         {
             List<string> inputLines = System.IO.File.ReadAllLines(@"..\..\input12.txt").ToList();
+
+            Dictionary<int, List<int>> keys = new Dictionary<int, List<int>>();
+
+            foreach(var input in inputLines)
+            {
+                string inp = input.Replace(" <-> ", ".");
+                List<string> codes = inp.Split('.').ToList();
+                List<int> values = codes[1].Split(',').Select(x => int.Parse(x)).ToList();
+                for(int i = 0; i < values.Count; i++)
+                {
+                    if(!keys.ContainsKey(int.Parse(codes[0])))
+                    {
+                        keys.Add(int.Parse(codes[0]), new List<int> { values[i] });
+                    }
+                    else
+                    {
+                        keys[int.Parse(codes[0])].Add(values[i]);
+                    }
+                }
+            }
+
+            int count = 0;
+            List<int> programs = new List<int>();
+            programs.Add(0);
+            while(count != programs.Count)
+            {
+                count = programs.Count;
+
+                foreach(var p in new List<int>(programs))
+                {
+                    foreach(var value in keys[p])
+                    {
+                        if (!programs.Contains(value))
+                            programs.Add(value);
+                    }
+                }
+            }
+            Console.WriteLine(programs.Count);
         }
 
         public void RunTwo()
         {
             List<string> inputLines = System.IO.File.ReadAllLines(@"..\..\input12.txt").ToList();
+
+            Dictionary<int, List<int>> keys = new Dictionary<int, List<int>>();
+
+            foreach (var input in inputLines)
+            {
+                string inp = input.Replace(" <-> ", ".");
+                List<string> codes = inp.Split('.').ToList();
+                List<int> values = codes[1].Split(',').Select(x => int.Parse(x)).ToList();
+                for (int i = 0; i < values.Count; i++)
+                {
+                    if (!keys.ContainsKey(int.Parse(codes[0])))
+                    {
+                        keys.Add(int.Parse(codes[0]), new List<int> { values[i] });
+                    }
+                    else
+                    {
+                        keys[int.Parse(codes[0])].Add(values[i]);
+                    }
+                }
+            }
+
+            List<string> groups = new List<string>();
+            List<int> covered = new List<int>();
+            foreach(var k in keys.Keys)
+            {
+                if (covered.Contains(k))
+                    continue;
+                int count = 0;
+                List<int> programs = new List<int>();
+                programs.Add(k);
+                while (count != programs.Count)
+                {
+                    count = programs.Count;
+
+                    foreach (var p in new List<int>(programs))
+                    {
+                        foreach (var value in keys[p])
+                        {
+                            if (!programs.Contains(value))
+                                programs.Add(value);
+                        }
+                    }
+                }
+                string test = "";
+                programs.Sort();
+                for(int j = 0; j < programs.Count; j++)
+                {
+                    test += programs[j];
+                    if (!covered.Contains(programs[j]))
+                        covered.Add(programs[j]);
+                }
+
+                if (!groups.Contains(test))
+                {
+                    groups.Add(test);
+                }
+            }
+            Console.WriteLine(groups.Count);
         }
     }
 }
--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -8,51 +8,79 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Advent of Code 2017!\n");
-
-            List<IPuzzle> puzzles = new List<IPuzzle>
+            while (true)
             {
-                new Day01(),
-                new Day02(),
-                new Day03(),
-                new Day04(),
-                new Day05(),
-                new Day06(),
-                new Day07(),
-                new Day08(),
-                new Day09(),
-                new Day10(),
-                new Day11(),
-                //new Day12(),
-                //new Day13(),
-                //new Day14(),
-                //new Day15(),
-                //new Day16(),
-                //new Day17(),
-                //new Day18(),
-                //new Day19(),
-                //new Day20(),
-                //new Day21(),
-                //new Day22(),
-                //new Day23(),
-                //new Day24(),
-                //new Day25(),
-            };
+                Console.WriteLine("Advent of Code 2017!\n");
 
-            int num = 1;
+                Console.WriteLine("Press 0 to quit");
+                Console.WriteLine("Press 1 to run all");
+                Console.WriteLine("Press 2 to run specific");
+                Console.WriteLine("Press 3 to run specific x times");
+                Console.WriteLine("Press 4 to benchmark all puzzles");
 
-            foreach(var p in puzzles)
-            {
-                Console.WriteLine("Solution to puzzle {0}", num++);
-                Console.Write("a) ");
-                TimeTask(() => p.RunOne());
-                Console.Write("b) ");
-                TimeTask(() => p.RunTwo());
-                Console.WriteLine();
+                Console.WriteLine("\n");
+
+                var key = Console.ReadKey();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.D0:
+                        return;
+
+                    case ConsoleKey.D1:
+                        List<IPuzzle> puzzles = new List<IPuzzle>
+                    {
+                        new Day01(),
+                        new Day02(),
+                        new Day03(),
+                        new Day04(),
+                        new Day05(),
+                        new Day06(),
+                        new Day07(),
+                        new Day08(),
+                        new Day09(),
+                        new Day10(),
+                        new Day11(),
+                        new Day12(),
+                        //new Day13(),
+                        //new Day14(),
+                        //new Day15(),
+                        //new Day16(),
+                        //new Day17(),
+                        //new Day18(),
+                        //new Day19(),
+                        //new Day20(),
+                        //new Day21(),
+                        //new Day22(),
+                        //new Day23(),
+                        //new Day24(),
+                        //new Day25(),
+                    };
+
+                        int num = 1;
+
+                        foreach (var p in puzzles)
+                        {
+                            Console.WriteLine("Solution to puzzle {0}", num++);
+                            Console.Write("a) ");
+                            TimeTask(() => p.RunOne());
+                            Console.Write("b) ");
+                            TimeTask(() => p.RunTwo());
+                            Console.WriteLine();
+                        }
+                        break;
+
+                    case ConsoleKey.D2:
+                        break;
+
+                    case ConsoleKey.D3:
+                        break;
+                }
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
             }
-
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
         }
 
         static void TimeTask(Action function)
