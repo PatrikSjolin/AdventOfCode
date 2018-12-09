e988316601daea3bfb3f359ecf53a33dbf8ef323
Sealion <patrik.sjolin@gmail.com>
2018-12-09 12:20:21 +0100

    Adding more benchmarking

--- AdventOfCode/AdventOfCode/Days 2018/Day09.cs
+++ AdventOfCode/AdventOfCode/Days 2018/Day09.cs
@@ -13,56 +13,7 @@ namespace AdventOfCode.Days_2018
             int numPlayers = 426;
             int lastMarbleScore = 72058;
 
-            Dictionary<long, long> playerScores = new Dictionary<long, long>();
-
-            for (int i = 0; i < numPlayers; i++)
-            {
-                playerScores.Add(i, 0);
-            }
-
-            int score = 0;
-            int marble = 0;
-            int playerTurn = 0;
-
-            LinkedList<int> marbles = new LinkedList<int>();
-            LinkedListNode<int> current = marbles.AddFirst(marble);
-            marble++;
-
-            while (marble != lastMarbleScore)
-            {
-                var nextNode = current.Next;
-                if (nextNode == null)
-                {
-                    nextNode = marbles.First;
-                }
-                current = marbles.AddAfter(nextNode, marble);
-                marble++;
-                playerTurn++;
-                playerTurn = playerTurn % numPlayers;
-
-                if (marble % 23 == 0)
-                {
-                    score = marble;
-                    for (int i = 0; i < 7; i++)
-                    {
-                        current = current.Previous;
-                        if (current == null)
-                        {
-                            current = marbles.Last;
-                        }
-                    }
-
-                    int m = current.Value;
-                    nextNode = current.Next;
-                    marbles.Remove(current);
-                    current = nextNode;
-                    score += m;
-                    playerScores[playerTurn] += score;
-                    marble++;
-                    playerTurn++;
-                    playerTurn = playerTurn % numPlayers;
-                }
-            }
+            Dictionary<long, long> playerScores = GetPlayerScores2(numPlayers, lastMarbleScore);
 
             long highest = 0;
 
@@ -77,43 +28,12 @@ namespace AdventOfCode.Days_2018
             return highest.ToString();
         }
 
-        public class LinkedNode
-        {
-            public LinkedNode Previous { get; set; }
-            public LinkedNode Next { get; set; }
-        }
-
-        public class LinkedNodeList
-        {
-            public LinkedNode AddFirst(int value)
-            {
-
-            }
-
-            public void AddAfter(LinkedNode n, int value)
-            {
-                LinkedNode next = n.Next;
-                LinkedNode newNode = new LinkedNode();
-
-                LinkedNode previous = n.Previous;
-
-                previous.Next = newNode;
-                newNode.Next = next;
-                next.Previous = newNode;
-            }
-
-            public LinkedNode Remove(LinkedNode node)
-            {
-                return null;
-            }
-        }
-
         public string RunTwo()
         {
             int numPlayers = 426;
             int lastMarbleScore = 72058 * 100;
 
-            Dictionary<long, long> playerScores = GetPlayerScores(numPlayers, lastMarbleScore);
+            Dictionary<long, long> playerScores = GetPlayerScores2(numPlayers, lastMarbleScore);
 
             long highest = 0;
 
@@ -184,6 +104,50 @@ namespace AdventOfCode.Days_2018
             return playerScores;
         }
 
+        public class LinkedNode
+        {
+            public LinkedNode Previous { get; set; }
+            public LinkedNode Next { get; set; }
+            public int Value { get; set; }
+        }
+
+        public static class LinkedNodeList
+        {
+            public static LinkedNode AddFirst(int value)
+            {
+                LinkedNode ln = new LinkedNode();
+
+                ln.Value = value;
+                ln.Previous = ln;
+                ln.Next = ln;
+
+                return ln;
+            }
+
+            public static LinkedNode AddAfter(LinkedNode n, int value)
+            {
+                LinkedNode next = n.Next;
+                LinkedNode newNode = new LinkedNode();
+                newNode.Value = value;
+
+                LinkedNode previous = n;
+
+                previous.Next = newNode;
+                newNode.Next = next;
+                next.Previous = newNode;
+                newNode.Previous = previous;
+
+                return newNode;
+            }
+
+            public static void Remove(LinkedNode node)
+            {
+                node.Previous.Next = node.Next;
+                node.Next.Previous = node.Previous;
+                node = null;
+            }
+        }
+
         private Dictionary<long, long> GetPlayerScores2(int numPlayers, int lastMarbleScore)
         {
             Dictionary<long, long> playerScores = new Dictionary<long, long>();
@@ -197,40 +161,29 @@ namespace AdventOfCode.Days_2018
             int marble = 0;
             int playerTurn = 0;
 
-            LinkedNodeList marbles = new LinkedNodeList();
-
-            LinkedNode current = marbles.AddFirst(marble);
+            LinkedNode current = LinkedNodeList.AddFirst(marble);
             marble++;
 
             while (marble != lastMarbleScore)
             {
-                var nextNode = current.Next;
-                if (nextNode == null)
-                {
-                    nextNode = marbles.First;
-                }
-                current = marbles.AddAfter(nextNode, marble);
+                LinkedNode nextNode = current.Next;
+                current = LinkedNodeList.AddAfter(nextNode, marble);
                 marble++;
                 playerTurn++;
                 playerTurn = playerTurn % numPlayers;
 
                 if (marble % 23 == 0)
                 {
-                    score = marble;
                     for (int i = 0; i < 7; i++)
                     {
                         current = current.Previous;
-                        if (current == null)
-                        {
-                            current = marbles.Last;
-                        }
                     }
 
-                    int m = current.Value;
+                    score = marble + current.Value;
                     nextNode = current.Next;
-                    marbles.Remove(current);
+                    LinkedNodeList.Remove(current);
                     current = nextNode;
-                    score += m;
+
                     playerScores[playerTurn] += score;
                     marble++;
                     playerTurn++;
--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -15,7 +15,7 @@ namespace AdventOfCode
             List<string> zeroStars2017 = new List<string>();
             List<string> zeroStars2018 = new List<string>();
 
-            foreach(var v in input2017)
+            foreach (var v in input2017)
             {
                 List<string> splitList = v.Split(' ').ToList();
                 if (splitList[4] == "0")
@@ -39,7 +39,7 @@ namespace AdventOfCode
                 }
             }
 
-            foreach(var v in zeroStars2017)
+            foreach (var v in zeroStars2017)
             {
                 if (zeroStars2018.Contains(v))
                 {
@@ -67,6 +67,8 @@ namespace AdventOfCode
                 Console.WriteLine("Press 1 to run all");
                 Console.WriteLine("Press 2 to run specific");
                 Console.WriteLine("Press 3 to benchmark all puzzles");
+                Console.WriteLine("Press 4 to benchmark all puzzles (detailed)");
+                Console.WriteLine("Press 5 to benchmark specific puzzles");
 
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write("\nYour input: ");
@@ -145,6 +147,67 @@ namespace AdventOfCode
                             Console.ForegroundColor = ConsoleColor.White;
                             break;
                         }
+                    case ConsoleKey.D4:
+                    case ConsoleKey.NumPad4:
+                        {
+                            Console.Write("\nEnter number of runs: ");
+
+                            int runs = int.Parse(Console.ReadLine());
+                            Dictionary<int, Point> times = new Dictionary<int, Point>();
+
+                            foreach (var p in puzzles)
+                            {
+                                Point point = new Point(0, 0);
+                                for (int i = 0; i < runs; i++)
+                                {
+                                    point.X = TimeTask(() => p.Value.RunOne(), true);
+                                    point.Y = TimeTask(() => p.Value.RunTwo(), true);
+                                }
+                                times.Add(p.Key, point);
+                            }
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\nAverage times after {0} runs:", runs);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            foreach (var time in times)
+                            {
+                                Console.Write("Puzzle {0}: {1} ms", time.Key < 10 ? ("0" + time.Key) : time.Key.ToString(), (time.Value.X + time.Value.Y) / runs);
+                                Console.Write(" (A) {0} ms, ", time.Value.X);
+                                Console.WriteLine("B) {0} ms)", time.Value.Y);
+                            }
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\nTotal time: {0} ms", times.Values.Sum(x => x.X + x.Y) / runs);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                        }
+                    case ConsoleKey.D5:
+                    case ConsoleKey.NumPad5:
+                        {
+                            Console.Write("\nEnter puzzle number: ");
+                            int puzzleNumber = int.Parse(Console.ReadLine());
+
+
+                            Console.Write("\nEnter number of runs: ");
+                            int runs = int.Parse(Console.ReadLine());
+                            List<Point> times = new List<Point>();
+
+                            for(int i = 0; i < runs; i++)
+                            {
+                                Point p = new Point(0, 0);
+                                p.X = TimeTask(() => puzzles[puzzleNumber].RunOne(), true);
+                                p.Y = TimeTask(() => puzzles[puzzleNumber].RunTwo(), true);
+                                times.Add(p);
+                            }
+
+                            int sumX = times.Sum(x => x.X);
+                            int sumY = times.Sum(x => x.Y);
+
+                            Console.WriteLine("\nAverage times after {0} runs:", runs);
+                            Console.WriteLine("A: {0} ms", sumX / runs);
+                            Console.WriteLine("B: {0} ms", sumY / runs);
+                            Console.WriteLine();
+                            break;
+                        }
                 }
 
                 Console.WriteLine("\nPress any key to continue...");
@@ -155,7 +218,7 @@ namespace AdventOfCode
 
         private static Dictionary<int, IPuzzle> GetPuzzles(int year)
         {
-            if(year == 2015)
+            if (year == 2015)
             {
                 return new Dictionary<int, IPuzzle>
                 {
@@ -199,7 +262,7 @@ namespace AdventOfCode
                     { 25, new Day25() }
                 };
             }
-            if(year == 2018)
+            if (year == 2018)
             {
                 return new Dictionary<int, IPuzzle>
                 {
@@ -212,6 +275,7 @@ namespace AdventOfCode
                     { 7, new Days_2018.Day07() },
                     { 8, new Days_2018.Day08() },
                     { 9, new Days_2018.Day09() },
+                    //{ 10, new Days_2018.Day10() },
                 };
             }
             return null;
