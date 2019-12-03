776b6561926c7dc2b2e9a0bf1f5c561f9b5902f6
PatrikSjolin <patrik.sjolin@gmail.com>
2019-12-03 18:43:43 +0100

    Solution to day 3

--- AdventOfCode/AdventOfCode/Days 2019/Day03.cs
+++ AdventOfCode/AdventOfCode/Days 2019/Day03.cs
@@ -8,16 +8,267 @@ namespace AdventOfCode.Days_2019
 {
     public class Day03 : IPuzzle
     {
-        public bool Active => false;
+        public bool Active => true;
 
         public string RunOne()
         {
-            throw new NotImplementedException();
+            List<string> startInput = System.IO.File.ReadAllLines(@"..\..\Data\2019\input03.txt").ToList();
+            List<string> wire1 = startInput[0].Split(',').ToList();
+            List<string> wire2 = startInput[1].Split(',').ToList();
+
+            HashSet<Point> points = new HashSet<Point>();
+            
+            int x = 0;
+            int y = 0;
+
+            foreach(var v in wire1)
+            {
+                string direction = v.First().ToString();
+                int moves = int.Parse(v.Substring(1, v.Length - 1));
+
+                if(direction == "R")
+                {
+                    for(int i = 0; i < moves; i++)
+                    {
+                        x++;
+                        points.Add(new Point(x, y));
+                    }
+                }
+                if (direction == "U")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        y++;
+                        points.Add(new Point(x, y));
+                    }
+                }
+                if (direction == "L")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        x--;
+                        points.Add(new Point(x, y));
+                    }
+                }
+                if (direction == "D")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        y--;
+                        points.Add(new Point(x, y));
+                    }
+                }
+            }
+
+            x = 0;
+            y = 0;
+
+            int distance = int.MaxValue;
+
+            foreach (var v in wire2)
+            {
+                string direction = v.First().ToString();
+                int moves = int.Parse(v.Substring(1, v.Length - 1));
+
+                if (direction == "R")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        x++;
+                        if(points.Contains(new Point(x, y)))
+                        {
+                            int manhattan = Math.Abs(x) + Math.Abs(y);
+                            if (manhattan < distance)
+                                distance = manhattan;
+                        }
+                    }
+                }
+                if (direction == "U")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        y++;
+                        if (points.Contains(new Point(x, y)))
+                        {
+                            int manhattan = Math.Abs(x) + Math.Abs(y);
+                            if (manhattan < distance)
+                                distance = manhattan;
+                        }
+                    }
+                }
+                if (direction == "L")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        x--;
+                        if (points.Contains(new Point(x, y)))
+                        {
+                            int manhattan = Math.Abs(x) + Math.Abs(y);
+                            if (manhattan < distance)
+                                distance = manhattan;
+                        }
+                    }
+                }
+                if (direction == "D")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        y--;
+                        if (points.Contains(new Point(x, y)))
+                        {
+                            int manhattan = Math.Abs(x) + Math.Abs(y);
+                            if (manhattan < distance)
+                                distance = manhattan;
+                        }
+                    }
+                }
+
+
+            }
+
+            return distance.ToString();
         }
 
         public string RunTwo()
         {
-            throw new NotImplementedException();
+            List<string> startInput = System.IO.File.ReadAllLines(@"..\..\Data\2019\input03.txt").ToList();
+            List<string> wire1 = startInput[0].Split(',').ToList();
+            List<string> wire2 = startInput[1].Split(',').ToList();
+
+            HashSet<Point> points = new HashSet<Point>();
+            Dictionary<Point, int> pointSteps = new Dictionary<Point, int>();
+
+            int x = 0;
+            int y = 0;
+            int steps = 0;
+
+            foreach (var v in wire1)
+            {
+                string direction = v.First().ToString();
+                int moves = int.Parse(v.Substring(1, v.Length - 1));
+
+                if (direction == "R")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        steps++;
+                        x++;
+                        Point p;
+                        points.Add(p = new Point(x, y));
+                        if(!pointSteps.ContainsKey(p))
+                            pointSteps.Add(p, steps);
+                    }
+                }
+                if (direction == "U")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        steps++;
+                        y++;
+                        Point p;
+                        points.Add(p = new Point(x, y));
+                        if (!pointSteps.ContainsKey(p))
+                            pointSteps.Add(p, steps);
+                    }
+                }
+                if (direction == "L")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        steps++;
+                        x--;
+                        Point p;
+                        points.Add(p = new Point(x, y));
+                        if (!pointSteps.ContainsKey(p))
+                            pointSteps.Add(p, steps);
+                    }
+                }
+                if (direction == "D")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        steps++;
+                        y--;
+                        Point p;
+                        points.Add(p = new Point(x, y));
+                        if (!pointSteps.ContainsKey(p))
+                            pointSteps.Add(p, steps);
+                    }
+                }
+            }
+
+            x = 0;
+            y = 0;
+            steps = 0;
+
+            int distance = int.MaxValue;
+
+            foreach (var v in wire2)
+            {
+                string direction = v.First().ToString();
+                int moves = int.Parse(v.Substring(1, v.Length - 1));
+
+                if (direction == "R")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        steps++;
+                        x++;
+                        if (points.Contains(new Point(x, y)))
+                        {
+                            int manhattan = steps + pointSteps[new Point(x, y)];
+                            if (manhattan < distance)
+                                distance = manhattan;
+                        }
+                    }
+                }
+                if (direction == "U")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        steps++;
+                        y++;
+                        if (points.Contains(new Point(x, y)))
+                        {
+                            int manhattan = steps + pointSteps[new Point(x, y)];
+                            if (manhattan < distance)
+                                distance = manhattan;
+                        }
+                    }
+                }
+                if (direction == "L")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        steps++;
+                        x--;
+                        if (points.Contains(new Point(x, y)))
+                        {
+                            int manhattan = steps + pointSteps[new Point(x, y)];
+                            if (manhattan < distance)
+                                distance = manhattan;
+                        }
+                    }
+                }
+                if (direction == "D")
+                {
+                    for (int i = 0; i < moves; i++)
+                    {
+                        steps++;
+                        y--;
+                        if (points.Contains(new Point(x, y)))
+                        {
+                            int manhattan = steps + pointSteps[new Point(x, y)];
+                            if (manhattan < distance)
+                                distance = manhattan;
+                        }
+                    }
+                }
+
+
+            }
+
+            return distance.ToString();
         }
     }
 }
