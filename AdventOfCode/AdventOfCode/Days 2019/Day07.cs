f3d7e2bf6dea61e80699a5297b6b56b5fe667111
Sealion <patrik.sjolin@gmail.com>
2019-12-08 22:28:49 +0100

    Solution day 7 and 8

--- AdventOfCode/AdventOfCode/Days 2019/Day07.cs
+++ AdventOfCode/AdventOfCode/Days 2019/Day07.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode.Days_2019
 {
@@ -20,7 +18,7 @@ namespace AdventOfCode.Days_2019
 
         public string RunOne()
         {
-            List<int> inputs2 = System.IO.File.ReadAllLines(@"..\..\Data\2019\input07test.txt")[0].Split(',').Select(x => int.Parse(x)).ToList();
+            List<int> inputs2 = System.IO.File.ReadAllLines(@"..\..\Data\2019\input07.txt")[0].Split(',').Select(x => int.Parse(x)).ToList();
 
             Tuple<int, List<int>> maxSetting = new Tuple<int, List<int>>(int.MinValue, new List<int>());
 
@@ -30,21 +28,26 @@ namespace AdventOfCode.Days_2019
             {
                 for(int j = 0; j <= 4; j++)
                 {
+                    if (i == j)
+                        continue;
                     for(int k = 0; k <= 4; k++)
                     {
+                        if (k == i || k == j)
+                            continue;
                         for(int l = 0; l <= 4; l++)
                         {
+                            if (l == k || l == j || l == i)
+                                continue;
                             for(int a = 0; a <= 4; a++)
                             {
+                                if (a == l || a == k || a == j || a == i)
+                                    continue;
                                 phaseSettings.Add(new List<int> { i, j, k, l, a });
                             }
                         }
                     }
                 }
             }
-
-            //phaseSettings = new List<List<int>> { new List<int> { 4, 3, 2, 1, 0 } };
-
 
             foreach (var setting in phaseSettings)
             {
@@ -105,7 +108,7 @@ namespace AdventOfCode.Days_2019
                         }
                         else if (op == 4)
                         {
-                            output = inputs[inputs[i + 1]];
+                            output = GetValue(inputs, i + 1, par1);
                             if (j == 4 && output > maxSetting.Item1)
                                 maxSetting = new Tuple<int, List<int>>(output, setting);
                             break;
@@ -167,9 +170,190 @@ namespace AdventOfCode.Days_2019
             return maxSetting.Item1.ToString();
         }
 
+        public class State
+        {
+            public int Pointer { get; set; }
+            public List<int> Register { get; set; }
+            public int LastOutput { get; set; }
+        }
+
         public string RunTwo()
         {
-            throw new NotImplementedException();
+            List<int> inputs2 = System.IO.File.ReadAllLines(@"..\..\Data\2019\input07.txt")[0].Split(',').Select(x => int.Parse(x)).ToList();
+
+            Tuple<int, List<int>> maxSetting = new Tuple<int, List<int>>(int.MinValue, new List<int>());
+
+            List<List<int>> phaseSettings = new List<List<int>>();
+
+            for (int i = 5; i <= 9; i++)
+            {
+                for (int j = 5; j <= 9; j++)
+                {
+                    if (i == j)
+                        continue;
+                    for (int k = 5; k <= 9; k++)
+                    {
+                        if (k == i || k == j)
+                            continue;
+                        for (int l = 5; l <= 9; l++)
+                        {
+                            if (l == k || l == j || l == i)
+                                continue;
+                            for (int a = 5; a <= 9; a++)
+                            {
+                                if (a == l || a == k || a == j || a == i)
+                                    continue;
+                                phaseSettings.Add(new List<int> { i, j, k, l, a });
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (var setting in phaseSettings)
+            {
+                int output = 0;
+
+                List<int> inputs = inputs2.ToList();
+
+                Dictionary<int, State> state = new Dictionary<int, State>
+                {
+                    { 0, new State { Pointer = 0, Register = inputs.ToList() } },
+                    { 1, new State { Pointer = 0, Register = inputs.ToList() } },
+                    { 2, new State { Pointer = 0, Register = inputs.ToList() } },
+                    { 3, new State { Pointer = 0, Register = inputs.ToList() } },
+                    { 4, new State { Pointer = 0, Register = inputs.ToList() } },
+                };
+
+                bool done = false;
+
+                for (int j = 0; j < setting.Count; j = (j + 1) % 5)
+                {
+                    for (int i = state[j].Pointer; i < state[j].Register.Count;)
+                    {
+                        string start = state[j].Register[i].ToString();
+                        int op = 0;
+                        int par1 = 0;
+                        int par2 = 0;
+
+                        if (start.Length == 1)
+                        {
+                            op = state[j].Register[i];
+                        }
+                        else if (start.Length == 4)
+                        {
+                            op = (int)char.GetNumericValue(start[3]);
+                            par1 = (int)char.GetNumericValue(start[1]);
+                            par2 = (int)char.GetNumericValue(start[0]);
+
+                        }
+                        else if (start.Length == 3)
+                        {
+                            op = (int)char.GetNumericValue(start[2]);
+                            par1 = (int)char.GetNumericValue(start[0]);
+                        }
+                        else
+                        {
+                            op = 99;
+                        }
+
+                        if (op == 1)
+                        {
+                            state[j].Register[state[j].Register[i + 3]] = GetValue(state[j].Register, i + 1, par1) + GetValue(state[j].Register, i + 2, par2);
+                            i += 4;
+                        }
+                        else if (op == 2)
+                        {
+                            state[j].Register[state[j].Register[i + 3]] = GetValue(state[j].Register, i + 1, par1) * GetValue(state[j].Register, i + 2, par2);
+                            i += 4;
+                        }
+                        else if (op == 3)
+                        {
+                            if (i == 0)
+                            {
+                                state[j].Register[state[j].Register[i + 1]] = setting[j];
+                            }
+                            else
+                            {
+                                state[j].Register[state[j].Register[i + 1]] = output;
+                            }
+                            i += 2;
+                        }
+                        else if (op == 4)
+                        {
+                            output = GetValue(state[j].Register, i + 1, par1);
+                            state[j].LastOutput = output;
+                            state[j].Pointer = i + 2;
+                            break;
+                        }
+                        else if (op == 5)
+                        {
+                            if (GetValue(state[j].Register, i + 1, par1) != 0)
+                            {
+                                i = GetValue(state[j].Register, i + 2, par2);
+                            }
+                            else
+                            {
+                                i += 3;
+                            }
+                        }
+                        else if (op == 6)
+                        {
+                            if (GetValue(state[j].Register, i + 1, par1) == 0)
+                            {
+                                i = GetValue(state[j].Register, i + 2, par2);
+                            }
+                            else
+                            {
+                                i += 3;
+                            }
+                        }
+                        else if (op == 7)
+                        {
+                            if (GetValue(state[j].Register, i + 1, par1) < GetValue(state[j].Register, i + 2, par2))
+                            {
+                                state[j].Register[state[j].Register[i + 3]] = 1;
+                            }
+                            else
+                            {
+                                state[j].Register[state[j].Register[i + 3]] = 0;
+                            }
+                            i += 4;
+                        }
+                        else if (op == 8)
+                        {
+                            if (GetValue(state[j].Register, i + 1, par1) == GetValue(state[j].Register, i + 2, par2))
+                            {
+                                state[j].Register[state[j].Register[i + 3]] = 1;
+                            }
+                            else
+                            {
+                                state[j].Register[state[j].Register[i + 3]] = 0;
+                            }
+                            i += 4;
+                        }
+                        else if (op == 99)
+                        {
+                            if(j == 4)
+                            {
+                                done = true;
+                            }
+                            break;
+                        }
+                    }
+
+                    if (done)
+                    {
+                        if(state[4].LastOutput > maxSetting.Item1)
+                        {
+                            maxSetting = new Tuple<int, List<int>>(state[4].LastOutput, setting);
+                        }
+                        break;
+                    }
+
+                }
+            }
+            return maxSetting.Item1.ToString();
         }
     }
 }
--- AdventOfCode/AdventOfCode/Days 2019/Day08.cs
+++ AdventOfCode/AdventOfCode/Days 2019/Day08.cs
@@ -1,23 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode.Days_2019
 {
     public class Day08 : IPuzzle
     {
-        public bool Active => false;
+        public bool Active => true;
 
         public string RunOne()
         {
-            throw new NotImplementedException();
+            int width = 25;
+            int height = 6;
+
+            int recordZeroes = int.MaxValue;
+
+            int recordLayer = -1;
+
+            int zeros = 0;
+            string inputs = System.IO.File.ReadAllLines(@"..\..\Data\2019\input08.txt").ToList()[0];
+
+            int start = 0;
+
+            for (int i = 0; i < inputs.Length;)
+            {
+                zeros = 0;
+                start = i;
+                for (int k = 0; k < height * width; k++)
+                {
+                    if (int.Parse(inputs[i].ToString()) == 0)
+                    {
+                        zeros++;
+                    }
+                    i++;
+                }
+                if (zeros < recordZeroes)
+                {
+                    recordZeroes = zeros;
+                    recordLayer = start;
+                }
+            }
+
+            int ones = 0;
+            int twos = 0;
+
+            for (int i = recordLayer; ; i++)
+            {
+                for (int k = 0; k < height * width; k++)
+                {
+                    if (int.Parse(inputs[i].ToString()) == 1)
+                    {
+                        ones++;
+                    }
+                    if (int.Parse(inputs[i].ToString()) == 2)
+                    {
+                        twos++;
+                    }
+                    i++;
+                }
+                break;
+            }
+
+            return (ones * twos).ToString();
         }
 
         public string RunTwo()
         {
-            throw new NotImplementedException();
+            int width = 25;
+            int height = 6;
+
+            string inputs = System.IO.File.ReadAllLines(@"..\..\Data\2019\input08.txt").ToList()[0];
+
+            int[,] grid = new int[25, 6];
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                    grid[j, i] = -1;
+            }
+
+            for (int i = 0; i < inputs.Length;)
+            {
+                for (int k = 0; k < height; k++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        if (grid[j, k] == -1 && int.Parse(inputs[i].ToString()) != 2)
+                            grid[j, k] = int.Parse(inputs[i].ToString());
+                        i++;
+                    }
+                }
+            }
+            //Console.WriteLine();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (grid[j, i] == 1)
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+                    //Console.Write(grid[j, i]);
+                }
+                //Console.WriteLine();
+            }
+
+            return "FAHEF";
         }
     }
 }
