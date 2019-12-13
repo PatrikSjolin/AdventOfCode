732c7059c35584aacd127100eae0e2985ebb8454
Sealion <patrik.sjolin@gmail.com>
2019-12-13 07:49:41 +0100

    Day 13 solution A

--- AdventOfCode/AdventOfCode/Days 2019/Day13.cs
+++ AdventOfCode/AdventOfCode/Days 2019/Day13.cs
@@ -10,14 +10,61 @@ namespace AdventOfCode.Days_2019
     {
         public bool Active => false;
 
+        Dictionary<Tuple<long, long>, long> gameBoard = new Dictionary<Tuple<long, long>, long>();
+
+        int i = 0;
+
         public string RunOne()
         {
-            throw new NotImplementedException();
+            List<long> inputs = System.IO.File.ReadAllLines(@"..\..\Data\2019\input13.txt")[0].Split(',').Select(x => long.Parse(x)).ToList();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                inputs.Add(0);
+            }
+
+
+            Computer c = new Computer(0);
+            c.OutputEvent += C_OutputEvent;
+            c.Compute(inputs.ToArray());
+
+            int sum = 0;
+
+            foreach(var tile in gameBoard)
+            {
+                if (tile.Value == 2)
+                    sum++;
+            }
+
+            return sum.ToString();
+        }
+
+        long x = 0;
+        long y = 0;
+
+        private void C_OutputEvent(object sender, EventArgs e)
+        {
+            Computer c = ((Computer)sender);
+            long output = c.Output;
+
+            if(i % 3 == 2)
+            {
+                gameBoard.Add(new Tuple<long, long>(x, y), output);
+            }
+            else if(i % 3 == 1)
+            {
+                y = output;
+            }
+            else
+            {
+                x = output;
+            }
+            i++;
         }
 
         public string RunTwo()
         {
-            throw new NotImplementedException();
+            return "";
         }
     }
 }
