f888a17bb74eda2103c7e84b696a8d9a6196116d
PatrikSjolin <patrik.sjolin@gmail.com>
2021-12-01 09:08:32 +0100

    Day 01

---
+++ AdventOfCode/AdventOfCode/Days 2021/Day01.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Days_2021
+{
+    public class Day01 : IPuzzle
+    {
+        public bool Active => true;
+
+        private List<int> inputs;
+
+        public string RunOne()
+        {
+            inputs = System.IO.File.ReadAllLines(@"..\..\Data\2021\input01.txt").Select(x => int.Parse(x)).ToList();
+            int larger = 0;
+            int previous = int.MaxValue;
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                if (inputs[i] > previous)
+                    larger++;
+                previous = inputs[i];
+            }
+
+            return larger.ToString();
+        }
+
+        public string RunTwo()
+        {
+            int larger = 0;
+            int previous = int.MaxValue;
+            for (int i = 0; i < inputs.Count - 2; i++)
+            {
+                if (inputs[i] + inputs[i + 1] + inputs[i + 2] > previous)
+                    larger++;
+                previous = inputs[i] + inputs[i + 1] + inputs[i + 2];
+            }
+
+            return larger.ToString();
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Days 2021/Day02.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days_2021
+{
+    public class Day02 : IPuzzle
+    {
+        public bool Active => true;
+
+        public string RunOne()
+        {
+            return "";
+        }
+
+        public string RunTwo()
+        {
+            return "";
+        }
+    }
+}
