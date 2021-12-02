d01c2ee9341cec847b342a1b69959a6e7f5f124b
PatrikSjolin <patrik.sjolin@gmail.com>
2021-12-02 08:58:37 +0100

    Day 02

--- AdventOfCode/AdventOfCode/Days 2021/Day02.cs
+++ AdventOfCode/AdventOfCode/Days 2021/Day02.cs
@@ -9,15 +9,53 @@ namespace AdventOfCode.Days_2021
     public class Day02 : IPuzzle
     {
         public bool Active => true;
+        private List<string> inputs;
 
         public string RunOne()
         {
-            return "";
+
+            inputs = System.IO.File.ReadAllLines(@"..\..\Data\2021\input02.txt").ToList();
+
+
+
+            int depth = 0;
+            int horizontal = 0;
+
+
+            for(int i = 0; i < inputs.Count; i++)
+            {
+                if (inputs[i].StartsWith("forward"))
+                    horizontal += int.Parse(inputs[i].Split(' ')[1]);
+                if(inputs[i].StartsWith("down"))
+                    depth += int.Parse(inputs[i].Split(' ')[1]);
+                if(inputs[i].StartsWith("up"))
+                    depth -= int.Parse(inputs[i].Split(' ')[1]);
+            }
+
+            return (horizontal * depth).ToString();
         }
 
         public string RunTwo()
         {
-            return "";
+            int depth = 0;
+            int horizontal = 0;
+            int aim = 0; 
+
+
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                if (inputs[i].StartsWith("forward"))
+                {
+                    horizontal += int.Parse(inputs[i].Split(' ')[1]);
+                    depth += aim * int.Parse(inputs[i].Split(' ')[1]);
+                }
+                if (inputs[i].StartsWith("down"))
+                    aim += int.Parse(inputs[i].Split(' ')[1]);
+                if (inputs[i].StartsWith("up"))
+                    aim -= int.Parse(inputs[i].Split(' ')[1]);
+            }
+
+            return (horizontal * depth).ToString();
         }
     }
 }
