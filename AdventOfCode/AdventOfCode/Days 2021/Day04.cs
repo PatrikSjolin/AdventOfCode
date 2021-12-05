55b6f76ce10523860f6a8203628920766584d433
PatrikSjolin <patrik.sjolin@gmail.com>
2021-12-05 17:16:40 +0100

    Day 4 and 5

---
+++ AdventOfCode/AdventOfCode/Days 2021/Day04.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Days_2021
+{
+    public class Day04 : IPuzzle
+    {
+        public bool Active => true;
+        private List<string> inputs;
+
+        public string RunOne()
+        {
+            inputs = System.IO.File.ReadAllLines(@"..\..\Data\2021\input04.txt").ToList();
+
+            List<int> numbers = inputs[0].Split(',').Select(x => int.Parse(x)).ToList();
+
+            Dictionary<int, List<List<int>>> boards = new Dictionary<int, List<List<int>>>();
+
+            List<int> board = new List<int>();
+            int index = -1;
+            for(int i = 1; i < inputs.Count; i++)
+            {
+                if(inputs[i] == "")
+                {
+                    index++;
+                    boards.Add(index, new List<List<int>>());
+                }
+                else
+                {                    
+                    List<string> split = inputs[i].Split(' ').ToList();
+                    split = split.Where(x => x != "").ToList();
+                    for(int j = 0; j < 5; j++)
+                    {
+                        board.Add(int.Parse(split[j]));
+                    }
+                    boards[index].Add(board);
+                    board = new List<int>();
+                }
+            }
+
+            
+            foreach(var b in boards)
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                    board = new List<int>();
+
+                    for (int j = 0; j < 5; j++)
+                    {
+                        board.Add(b.Value[j][i]);
+                    }
+
+                    b.Value.Add(board);
+                }                
+            }
+
+            HashSet<int> drawnNumbers = new HashSet<int>();
+
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                int number = numbers[i];
+                drawnNumbers.Add(number);
+
+                foreach(var b in boards)
+                {
+                    foreach(var line in b.Value)
+                    {
+                        bool won = true;
+                        for (int l = 0; l < 5; l++)
+                        {
+                            if (!drawnNumbers.Contains(line[l]))
+                                won = false;
+                        }
+
+                        if (won)
+                        {
+                            int sum = 0;
+                            for(int m = 0; m < 5; m++)
+                            {
+                                for(int n = 0; n < 5; n++)
+                                {
+                                    if (!drawnNumbers.Contains(b.Value[m][n]))
+                                        sum += b.Value[m][n];
+                                }
+                            }
+
+                            int result = sum * number;
+
+                            return result.ToString();
+                        }
+                    }
+                }
+
+            }
+
+            return "";
+        }
+
+        public string RunTwo()
+        {
+            List<int> numbers = inputs[0].Split(',').Select(x => int.Parse(x)).ToList();
+
+            Dictionary<int, List<List<int>>> boards = new Dictionary<int, List<List<int>>>();
+
+            List<int> victoriousBoards = new List<int>();
+
+            List<int> board = new List<int>();
+            int index = -1;
+            for (int i = 1; i < inputs.Count; i++)
+            {
+                if (inputs[i] == "")
+                {
+                    index++;
+                    boards.Add(index, new List<List<int>>());
+                }
+                else
+                {
+                    List<string> split = inputs[i].Split(' ').ToList();
+                    split = split.Where(x => x != "").ToList();
+                    for (int j = 0; j < 5; j++)
+                    {
+                        board.Add(int.Parse(split[j]));
+                    }
+                    boards[index].Add(board);
+                    board = new List<int>();
+                }
+            }
+
+
+            foreach (var b in boards)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    board = new List<int>();
+
+                    for (int j = 0; j < 5; j++)
+                    {
+                        board.Add(b.Value[j][i]);
+                    }
+
+                    b.Value.Add(board);
+                }
+            }
+
+            HashSet<int> drawnNumbers = new HashSet<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int number = numbers[i];
+                drawnNumbers.Add(number);
+
+                foreach (var b in boards)
+                {
+                    if (victoriousBoards.Contains(b.Key))
+                        continue;
+                    foreach (var line in b.Value)
+                    {
+                        bool won = true;
+                        for (int l = 0; l < 5; l++)
+                        {
+                            if (!drawnNumbers.Contains(line[l]))
+                                won = false;
+                        }
+
+                        if (won)
+                        {
+                            if (!victoriousBoards.Contains(b.Key))
+                            {
+                                victoriousBoards.Add(b.Key);
+                            }
+
+                            if (victoriousBoards.Count == boards.Count)
+                            {
+                                int sum = 0;
+                                for (int m = 0; m < 5; m++)
+                                {
+                                    for (int n = 0; n < 5; n++)
+                                    {
+                                        if (!drawnNumbers.Contains(b.Value[m][n]))
+                                            sum += b.Value[m][n];
+                                    }
+                                }
+
+                                int result = sum * number;
+
+                                return result.ToString();
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            return "";
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Days 2021/Day05.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Days_2021
+{
+    public class Day05 : IPuzzle
+    {
+        public bool Active => true;
+        List<string> inputs;
+
+        int gridSize = 1000;
+        public string RunOne()
+        {
+            inputs = System.IO.File.ReadAllLines(@"..\..\Data\2021\input05.txt").ToList();
+
+            int[,] grid = new int[gridSize, gridSize];
+            foreach (var line in inputs)
+            {
+                var points = line.Split('-');
+                points[0] = points[0].Replace(" ", "");
+                points[1] = points[1].Replace(" ", "");
+                points[1] = points[1].Replace(">", "");
+
+                int x1 = int.Parse(points[0].Split(',')[0]);
+                int y1 = int.Parse(points[0].Split(',')[1]);
+                int x2 = int.Parse(points[1].Split(',')[0]);
+                int y2 = int.Parse(points[1].Split(',')[1]);
+
+                if (y2 - y1 == 0)
+                {
+                    if (x2 > x1)
+                    {
+                        for (int i = x1; i <= x2; i++)
+                            grid[y2, i] += 1;
+                    }
+                    else
+                    {
+                        for (int i = x2; i <= x1; i++)
+                            grid[y2, i] += 1;
+                    }
+                }
+                if (x2 - x1 == 0)
+                {
+                    if (y2 > y1)
+                    {
+                        for (int i = y1; i <= y2; i++)
+                            grid[i, x2] += 1;
+                    }
+                    else
+                    {
+                        for (int i = y2; i <= y1; i++)
+                            grid[i, x2] += 1;
+                    }
+                }
+
+            }
+
+            int overlaps = 0;
+
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    if (grid[i, j] > 1)
+                        overlaps++;
+                }
+            }
+            return overlaps.ToString();
+        }
+
+        private void PrintGrid(int[,] grid)
+        {
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    Console.Write(grid[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public string RunTwo()
+        {
+            int[,] grid = new int[gridSize, gridSize];
+            foreach (var line in inputs)
+            {
+                var points = line.Split('-');
+                points[0] = points[0].Replace(" ", "");
+                points[1] = points[1].Replace(" ", "");
+                points[1] = points[1].Replace(">", "");
+
+                int x1 = int.Parse(points[0].Split(',')[0]);
+                int y1 = int.Parse(points[0].Split(',')[1]);
+                int x2 = int.Parse(points[1].Split(',')[0]);
+                int y2 = int.Parse(points[1].Split(',')[1]);
+
+                if (y2 - y1 == 0)
+                {
+                    if (x2 > x1)
+                    {
+                        for (int i = x1; i <= x2; i++)
+                            grid[y2, i] += 1;
+                    }
+                    else
+                    {
+                        for (int i = x2; i <= x1; i++)
+                            grid[y2, i] += 1;
+                    }
+                }
+                else if (x2 - x1 == 0)
+                {
+                    if (y2 > y1)
+                    {
+                        for (int i = y1; i <= y2; i++)
+                            grid[i, x2] += 1;
+                    }
+                    else
+                    {
+                        for (int i = y2; i <= y1; i++)
+                            grid[i, x2] += 1;
+                    }
+                }
+                else
+                {
+                    if (x1 > x2)
+                    {
+                        int tmpX = x2;
+                        int tmpY = y2;
+                        x2 = x1;
+                        y2 = y1;
+                        x1 = tmpX;
+                        y1 = tmpY;
+                    }
+                    int index = 0;
+                    for (int i = x1; i <= x2; i++)
+                    {
+                        grid[y1 + index, i] += 1;
+                        if (y1 > y2)
+                            index--;
+                        else
+                            index++;
+                    }
+
+                }
+            }
+
+            int overlaps = 0;
+
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    if (grid[i, j] > 1)
+                        overlaps++;
+                }
+            }
+            return overlaps.ToString();
+        }
+    }
+}
