980881b6bdf4ea304ebccbdb74cefaa2046fbbea
PatrikSjolin <patrik.sjolin@gmail.com>
2021-12-08 07:52:20 +0100

    Day 7 part 1

--- AdventOfCode/AdventOfCode/Days 2021/Day07.cs
+++ AdventOfCode/AdventOfCode/Days 2021/Day07.cs
@@ -13,20 +13,110 @@ namespace AdventOfCode.Days_2021
 
         public string RunOne()
         {
-            inputs = System.IO.File.ReadAllLines(@"..\..\Data\2021\input07test.txt")[0].Split(',').Select(x => int.Parse(x)).ToList();
+            inputs = System.IO.File.ReadAllLines(@"..\..\Data\2021\input07.txt")[0].Split(',').Select(x => int.Parse(x)).ToList();
 
-            inputs = inputs.OrderBy();
+            inputs.Sort();
+
 
             int fuelUsed = 0;
+            while (true)
+            {
+                int start = 0;
+                int end = 0;
 
+                int first = inputs[0];
+                int last = inputs.Last();
 
-            double hej = inputs.Average();
+                for(int i = 0; i < inputs.Count; i++)
+                {
+                    if (inputs[i] == first)
+                        start++;
+                }
+                for (int i = inputs.Count - 1 ; i >= 0; i--)
+                {
+                    if (inputs[i] == last)
+                        end++;
+                }
+
+                if (start == end && start == inputs.Count)
+                    return fuelUsed.ToString();
+
+                if(start < end)
+                {
+                    for(int i = 0; i < start; i++)
+                    {
+                        inputs[i]++;
+                        fuelUsed++;
+                    }
+                }
+                else
+                {
+                    for(int i = inputs.Count - 1; i > inputs.Count - 1 - end; i--)
+                    {
+                        inputs[i]--;
+                        fuelUsed++;
+                    }
+                }
+            }
 
             return "";
         }
 
         public string RunTwo()
         {
+            inputs = System.IO.File.ReadAllLines(@"..\..\Data\2021\input07test.txt")[0].Split(',').Select(x => int.Parse(x)).ToList();
+
+            inputs.Sort();
+
+            Dictionary<int, int> costs = new Dictionary<int, int>();
+
+            for(int i = 0; i < inputs.Count; i++)
+            {
+                costs.Add(i, 1);
+            }
+
+
+            int fuelUsed = 0;
+            while (true)
+            {
+                int start = 0;
+                int end = 0;
+
+                int first = inputs[0];
+                int last = inputs.Last();
+
+                for (int i = 0; i < inputs.Count; i++)
+                {
+                    if (inputs[i] == first)
+                        start++;
+                }
+                for (int i = inputs.Count - 1; i >= 0; i--)
+                {
+                    if (inputs[i] == last)
+                        end++;
+                }
+
+                if (start == end && start == inputs.Count)
+                    return fuelUsed.ToString();
+
+                if (start < end)
+                {
+                    for (int i = 0; i < start; i++)
+                    {
+                        inputs[i]++;
+                        fuelUsed++;
+                    }
+                }
+                else
+                {
+                    for (int i = inputs.Count - 1; i > inputs.Count - 1 - end; i--)
+                    {
+                        inputs[i]--;
+                        fuelUsed++;
+                    }
+                }
+            }
+
             return "";
         }
     }
